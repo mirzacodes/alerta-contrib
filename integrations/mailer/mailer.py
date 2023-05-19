023f25b0eee5345d55b0db9abde01e4054b1940b
mirzacodes <68922677+mirzacodes@users.noreply.github.com>
2023-05-19 19:45:36 +0500

    Update mailer.py

--- integrations/mailer/mailer.py
+++ integrations/mailer/mailer.py
@@ -209,7 +209,6 @@ class MailSender(threading.Thread):
                     continue
                 keep_alive = 0
             #keep_alive += 1
-            print("Working")
             time.sleep(2)
 
     def _rule_matches(self, regex, value):
